b32576d996b274140f9d98c7e74e707df481b747
KrystianGH2 <112473660+KrystianGH2@users.noreply.github.com>
2023-09-01 11:41:17 +0200

    added styling on skeleton loaders

--- components/pages/home/Feed.jsx
+++ components/pages/home/Feed.jsx
@@ -1,14 +1,19 @@
 "use client";
-import {useAllPosts} from "@/lib/my-api";
+import { useAllPosts } from "@/lib/my-api";
 import PostCard from "@/components/pages/home/PostCard";
-import {Skeleton} from "@/components/ui/skeleton";
+import { Skeleton } from "@/components/ui/skeleton";
 
 function Feed() {
-  const {status, error, data: postData = {posts: []}} = useAllPosts(); // default value for postData is an empty array
+  const { status, error, data: postData = { posts: [] } } = useAllPosts(); // default value for postData is an empty array
 
   if (status === "loading") {
     return (
-      <div className="w-full max-w-xl mt-12 flex flex-wrap gap-4 ">
+      <div className="flex flex-wrap justify-center gap-12 max-w-screen-2xl">
+        <Skeleton className="w-96  h-72" />
+        <Skeleton className="w-96  h-72" />
+        <Skeleton className="w-96 h-72" />
+        <Skeleton className="w-96 h-72" />
+        <Skeleton className="w-96 h-72" />
         <Skeleton className="w-96 h-72" />
       </div>
     );
